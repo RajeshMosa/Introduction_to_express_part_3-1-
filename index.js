e206d56e58ee66da583cc6a9555c1042ecfd32e8
RajeshMosa <mosarajesh11@gmail.com>
2024-03-06 08:38:13 +0000

    solve my pblm

--- index.js
+++ index.js
@@ -39,4 +39,50 @@ app.get('/books/', async (request, response) => {
 })
 
 //Get Book API
-app.get('/books/:bookId/', (request, response) => {})
+
+app.get('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+  const getBookQuery = `SELECT * FROM book WHERE book_id = ${bookId} `
+  const book = await db.get(getBookQuery)
+  response.send(book)
+})
+
+// ADD book
+
+app.post('/books/', async (request, response) => {
+  const bookDetails = request.body
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails
+  const addBookQuery = `
+    INSERT INTO
+      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+        '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        '${description}',
+         ${pages},
+        '${dateOfPublication}',
+        '${editionLanguage}',
+         ${price},
+        '${onlineStores}'
+      );`
+
+  const dbResponse = await db.run(addBookQuery)
+  const bookId = dbResponse.lastID
+  response.send({bookId: bookId})
+})
